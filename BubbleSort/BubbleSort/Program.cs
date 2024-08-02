59aa713b9f34b363fad75daf94c0894e5aafc1e5
Artem <st107472@student.spbu.ru>
2024-08-02 13:43:42 +0300

    Task Bubble sort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using Sorting;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter array size: ");
+
+        int arraySize;
+
+        while (!int.TryParse(Console.ReadLine(), out arraySize) || arraySize <= 0)
+        {
+            Console.WriteLine("Wrong size. Must be a positive number!");
+            Console.Write("Enter array size: ");
+        }
+
+        int[] numArray = new int[arraySize];
+
+        Console.WriteLine("Enter array elements one by one:");
+
+        for (int i = 0; i < arraySize ; ++i)
+        {
+            while (!int.TryParse(Console.ReadLine(), out numArray[i]))
+            {
+                Console.WriteLine("Wrong value!");
+                Console.Write("Enter another value: ");
+            }
+        }
+
+        Sort.BubbleSort(numArray);
+
+        for (int i = 0; i < numArray.Length; ++i)
+        {
+            Console.Write(numArray[i] + " ");
+        }
+    }
+}
---
+++ BubbleSort/BubbleSortTests/SortTest.cs
@@ -0,0 +1,36 @@
+﻿using Sorting;
+
+
+namespace SortingTests
+{
+    [TestClass()]
+    public class SortTest
+    {
+        [TestMethod()]
+        public void BubbleSortTestWithNegativeNumbers()
+        {
+            int[] arrayToSort = { 7, 8, 5, 4, 100, 11, 3, 2, 1, -5, -17 };
+            int[] sortedArray = { -17, -5, 1, 2, 3, 4, 5, 7, 8, 11, 100 };
+            Sort.BubbleSort(arrayToSort);
+            CollectionAssert.AreEqual(arrayToSort, sortedArray);
+        }
+
+        [TestMethod()]
+        public void BubbleSortTestWithAlreadySortedArray()
+        {
+            int[] arrayToSort = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            Sort.BubbleSort(arrayToSort);
+            CollectionAssert.AreEqual(arrayToSort, sortedArray);
+        }
+
+        [TestMethod()]
+        public void BubbleSortTestWithEmptyArray()
+        {
+            int[] arrayToSort = new int[0];
+            int[] sortedArray = {  };
+            Sort.BubbleSort(arrayToSort);
+            CollectionAssert.AreEqual(arrayToSort, sortedArray);
+        }
+    }
+}
